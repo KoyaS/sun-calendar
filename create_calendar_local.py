3f9ae1a1ff72525783aea53924302416b2bf905c
Avery2 <justinaverychan@gmail.com>
2023-10-29 11:44:36 -0700

    feat: create calendar

--- create_calendar_local.py
+++ create_calendar_local.py
@@ -1,7 +1,9 @@
 import requests
+from ics import Calendar, Event
+from datetime import datetime
 
 # API call for sunrise and sunset times in San Francisco, CA
-sun_times_url = "https://api.sunrise-sunset.org/json?lat=37.7749&lng=122.4194"
+sun_times_url = "https://api.sunrise-sunset.org/json?lat=37.7749000&lng=122.4194000&date=today"
 
 response = requests.get(sun_times_url)
 
@@ -10,3 +12,18 @@ if response.status_code == 200:
     print(data)
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+# convert from Coordinated Universal Time (UTC) to Pacific Daylight Time (PDT)
+
+current_date = datetime.now().strftime('%Y-%m-%d')
+sunrise_time = response.json().get('results').get('sunrise')
+sunrise_time = sunrise_time.split(' ')[0] # remove AM/PM
+
+c = Calendar()
+e = Event()
+e.name = "Sunrise"
+e.begin = f"{current_date} {sunrise_time}"
+c.events.add(e)
+
+with open('sun.ics', 'w') as f:
+    f.writelines(c.serialize_iter())
