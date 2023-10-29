1c8ac5db6fc97ed3e468f9961a725bbfd5620bd9
Avery2 <justinaverychan@gmail.com>
2023-10-29 11:51:41 -0700

    feat: convert to PDT timezone

--- create_calendar_local.py
+++ create_calendar_local.py
@@ -1,6 +1,7 @@
 import requests
 from ics import Calendar, Event
 from datetime import datetime
+import pytz
 
 # API call for sunrise and sunset times in San Francisco, CA
 sun_times_url = "https://api.sunrise-sunset.org/json?lat=37.7749000&lng=122.4194000&date=today"
@@ -13,16 +14,25 @@ if response.status_code == 200:
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
 
-# convert from Coordinated Universal Time (UTC) to Pacific Daylight Time (PDT)
 
 current_date = datetime.now().strftime('%Y-%m-%d')
-sunrise_time = response.json().get('results').get('sunrise')
-sunrise_time = sunrise_time.split(' ')[0] # remove AM/PM
+sunrise_time_utc = response.json().get('results').get('sunrise')
+
+# convert from Coordinated Universal Time (UTC) to Pacific Daylight Time (PDT)
+utc_zone = pytz.utc
+pdt_zone = pytz.timezone('America/Los_Angeles')
+utc_time_format = "%I:%M:%S %p"
+
+utc_time = datetime.strptime(sunrise_time_utc, utc_time_format).replace(tzinfo=utc_zone)
+
+pdt_time = utc_time.astimezone(pdt_zone)
+
+final_sunrise_time = pdt_time.strftime("%I:%M:%S")
 
 c = Calendar()
 e = Event()
 e.name = "Sunrise"
-e.begin = f"{current_date} {sunrise_time}"
+e.begin = f"{current_date} {final_sunrise_time}"
 c.events.add(e)
 
 with open('sun.ics', 'w') as f:
